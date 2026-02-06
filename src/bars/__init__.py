c854af45b7c0daaf4f22a38042d6f0685ba7e79d
Huberkeep <anithce@outlook.com>
2026-02-06 16:41:25 +0800

    Delete src/bars directory

--- src/bars/__init__.py
+++
@@ -1,9 +0,0 @@
-from .firatbars import ceicfirmbar,icrinputbar
-__all__ = ["ceicfirmbar","icrinputbar"]
-userInput = input("""Welcome!
-list:l
-empty option:q""")
-if userInput == l:
-  print("file firatbars.py")
-elif userInput == q:
-  pass
